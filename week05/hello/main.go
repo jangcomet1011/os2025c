2bcba257ee58af467e67cb4ef9f1cc0530fb85e7
Your jhs <jhs200697@naver.com>
2025-09-22 18:22:11 +0900

    v0.2 hello/main.go & wekk05.txt update

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	name := "Go"
-	fmt.Println("Hello", name)
+	//name := "Go"
+	//fmt.Println("Hello", name)
+
+	number := 7
+	fmt.Printf("number is %d", number)
 }
