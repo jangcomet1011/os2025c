310622bf154c470e6f7bf411ef2be9d7a2c4db75
Your jhs <jhs200697@naver.com>
2025-09-29 17:50:44 +0900

    v0.5 type/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -28,10 +28,23 @@ func main() {
 	//var name = "jallyyam"
 	//var gpa = 3.99
 
-	id := 999
-	name := "jallyyam"
-	gpa := 3.99
+	//id := 999
+	//name := "jallyyam"
+	//gpa := 3.99
 
-	fmt.Println("학번은 ", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
-	fmt.Println("평점 : ", gpa, reflect.TypeOf(gpa))
+	//fmt.Println("학번은 ", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
+	//fmt.Println("평점 : ", gpa, reflect.TypeOf(gpa))
+
+	// zero values
+	var f64 float64
+	var t bool
+	var s string
+	var i int
+	var i16 int16
+
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
+	fmt.Println(i16, reflect.TypeOf(i16))
 }
