9afa09a78ef0d4c11cfd11b06048bd06a20df8cc
Your jhs <jhs200697@naver.com>
2025-09-29 16:44:37 +0900

    v0.1 types/main.go

---
+++ week06/types/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Round(2.21))
+	fmt.Println(math.Ceil(2.21))
+	fmt.Println(strings.Title("go devlr"))
+
+}
