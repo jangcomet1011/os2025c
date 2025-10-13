acd4996b47bb3a5bf24dc279f42cdf0ea5e2fd75
Your jhs <jhs200697@naver.com>
2025-10-13 17:36:42 +0900

    loops/main.go v0.2 bufio & os package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n') // 에러를 무시함 "_" 는
+	//i, _ := r.ReadString('\n') // 에러를 무시함 "_" 는
+	i, err := r.ReadString('\n')
+	//fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) //report 에러 그리고 프로그램을 끝냄
+	}
+
 	fmt.Println(i)
 }
