b128e2b6f96dbd74d441b2845a28a7617001ee1a
Your jhs <jhs200697@naver.com>
2025-10-13 18:28:33 +0900

    반복문

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,47 +1,51 @@
+// guess challenges players to guess a random number.
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
 
-	fmt.Print("Enter a score: ")
-	r := bufio.NewReader(os.Stdin)
-	// i, _ := r.ReadString('\n') // ignore error
-	i, err := r.ReadString('\n')
-	// fmt.Println(err)
-	if err != nil {
-		log.Fatal(err) // report the error and exit the program
-		log.Fatal(err)
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
-	fmt.Println(i)
 
-	i = strings.TrimSpace(i)                // 문자열 주위에 붙은 공란 및 탭 키 등 제거
-	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
-	if err != nil {
-		log.Fatal(err)
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
 	}
-
-	if score >= 60 {
-		fmt.Println(score, "Pass")
-	} else {
-		fmt.Println(score, "Fail")
-	}
-
-	// shadowing
-	//var float64 float64 = 2.71
-	//var f float64 = 3.9991
-	//fmt.Println(float64)
-	//fmt.Println(f)
-
-	// 패키지 또는 정해진 타입의 함수명은 변수명으로 사용할수없제리얌
-	//var fmt float64 = 2.71
-	//fmt.Println(f)
-
 }
