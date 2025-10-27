100b9951390d076a51e2889897ae161d54d9245f
Your jhs <jhs200697@naver.com>
2025-10-27 17:49:09 +0900

    함수 포인터2

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
 
-func swap(first int, second int) {
+func swap(first *int, second *int) {
 	var temp int = 0
-	temp = first
-	first = second
-	second = temp
-	fmt.Println(first, second)
+	temp = *first
+	*first = *second
+	*second = temp
+	fmt.Println(*first, *second)
 }
 func main() {
 	//fmt.Printf("%0.3f\n", math.Sqrt(-25.0))
 	a, b := 10, 20
 	fmt.Println(a, b)
-	swap(a, b)
+	swap(&a, &b)
 	fmt.Println(a, b)
 }
