3ff5803f821bbb7c20fbdabab6d912e665505295
Your jhs <jhs200697@naver.com>
2025-10-27 18:06:34 +0900

    함수 패키지

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
 
 func swap(first *int, second *int) {
 	var temp int = 0
@@ -13,8 +33,20 @@ func swap(first *int, second *int) {
 }
 func main() {
 	//fmt.Printf("%0.3f\n", math.Sqrt(-25.0))
-	a, b := 10, 20
-	fmt.Println(a, b)
-	swap(&a, &b)
-	fmt.Println(a, b)
+	//a, b := 10, 20
+	//fmt.Println(a, b)
+	//swap(&a, &b)
+	//fmt.Println(a, b)
+	fmt.Println("점수입력")
+	score, err := GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	passFail := ""
+	if score >= 60 {
+		passFail = "pass!"
+	} else {
+		passFail = "Fail!"
+	}
+	fmt.Printf("%0.2f점은 %v\n", score, passFail)
 }
