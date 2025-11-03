8b8ebb5070be55804db6731256015c6d76dc8670
Your jhs <jhs200697@naver.com>
2025-11-03 18:11:19 +0900

    week10 array v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var arrayBool [3]bool
+	arrayBool := [3]bool{true, false, true} //array literal
 	var arrayInt [3]int
+	fmt.Println(reflect.TypeOf(arrayBool)) // [3]bool
+	fmt.Printf("%#v\n", arrayBool)         // [3]bool{true, false, true}
+
 	fmt.Println(arrayBool[1])
-	arrayInt[1]++
-	arrayInt[1] = arrayInt[1] + 1
+	arrayInt[1] = 2
 	fmt.Println(arrayInt[1])
 }
