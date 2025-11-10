a95a393d0a50ccee2958d2f00bd5205466a689a1
Your jhs <jhs200697@naver.com>
2025-11-10 17:58:44 +0900

    week11 slice v0.6

--- week11/cmd/ch05-ex02/main.go
+++ week11/cmd/ch05-ex02/main.go
@@ -1,30 +1,46 @@
 package main
 
-import
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func main() {
-	f, _ := os.open(scores.txt)
-}
-	func GetFloats(fileName string) ([]float64, error) {
-	var numbers []float64
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
 	file, err := os.Open(fileName)
-	if err != nil {	
+	if err != nil {
 		return numbers, err
 	}
+	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return numbers, err
 		}
-		numbers = append(numbers, number)
+		i++
 	}
 	err = file.Close()
 	if err != nil {
 		return numbers, err
 	}
 	if scanner.Err() != nil {
-		return nil, scanner.Err()
+		return numbers, scanner.Err()
 	}
 	return numbers, nil
+}
+func main() {
+	weights, err := GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sum := 0.0
+	for i := 0; i < len(weights); i++ {
+		sum = sum + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("평균 고기 소비량 : ", sum/weeks)
 }
