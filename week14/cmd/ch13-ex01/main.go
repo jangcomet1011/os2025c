b698265ea2e354f0848ce1d625705f588c76e6c0
Your jhs <jhs200697@naver.com>
2025-12-01 18:13:59 +0900

    week14 goroutine v0.3

--- week14/cmd/ch13-ex01/main.go
+++ week14/cmd/ch13-ex01/main.go
@@ -12,9 +12,15 @@ func say(msg string) {
 	}
 }
 
+func hi(msg string) {
+	//fmt.Println("안녕", msg)
+	time.Sleep(7 * time.Second)
+	fmt.Println("안녕", msg)
+}
 func main() {
 	start := time.Now()
-	go say("고루틴") // 새 고루틴에서 실행
-	say("메인")     // 메인 고루틴에서 실행
+	go hi("고루틴1")  // 새 고루틴에서 실행
+	go say("고루틴2") // 새고루틴에서 실행
+	time.Sleep(8 * time.Second)
 	fmt.Println("전체 실행 시간 : ", time.Since(start))
 }
